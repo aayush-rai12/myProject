e16a0df1839372a1e7ea14b15547f8d87e94b09c
Ayush Rai <ayushrai1210@gmail.com>
2023-05-26 17:04:39 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>OUT-OF-BOX</h1>
+        <p> &copy; all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target={"blank"}>
+            Youtube_
+          </a>
+          <a href="https://instagram.com" target={"blank"}>
+            Instagram_
+          </a>
+          <a href="https://github.com" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
